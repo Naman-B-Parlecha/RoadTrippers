875cf091fca3371e0340c341a28543b56667fb19
Naman-Parlecha <140307212+Naman-Parlecha@users.noreply.github.com>
2023-08-31 01:40:16 +0530

    Navigation and Screens

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Home Screen"),
+      ),
+    );
+  }
+}
