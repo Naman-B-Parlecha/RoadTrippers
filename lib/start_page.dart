10d1b9222afc7415f2751e32a881eb947e3dd504
Rishi Chirchi <rishiraj.chirchi@gmail.com>
2023-09-02 11:34:10 +0530

    Built a login page, sign up page .

---
+++ lib/start_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:loginpage/login_page.dart';
+
+class StartPage extends StatelessWidget {
+  final void Function() switchToLogin;
+  const StartPage(this.switchToLogin, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background/start.jpg'),
+              fit: BoxFit.fill),
+        ),
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 0, 0, 0),
+            Color.fromARGB(0, 0, 0, 0)
+          ], begin: Alignment.bottomCenter, end: Alignment.topCenter)),
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const SizedBox(
+                  height: 500,
+                ),
+                Text(
+                  "Come Explore With Us!",
+                  style: GoogleFonts.oswald(fontSize: 50, color: Colors.white),
+                  textAlign: TextAlign.left,
+                ),
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 230,
+                    ),
+                    Container(
+                      // color: Colors.black.withOpacity(0.2),
+                      child: ElevatedButton(
+                        onPressed: switchToLogin,
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                          backgroundColor:
+                              const Color.fromARGB(255, 78, 26, 126),
+                          foregroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 10),
+                        ),
+                        child: Text('Get Started'.toUpperCase()),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:road_trippers/main.dart';
+import 'package:loginpage/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
